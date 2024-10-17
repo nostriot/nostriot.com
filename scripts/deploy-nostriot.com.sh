fe0391a56b290fbdfad0184d169532faa76f1756
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2024-10-17 15:23:59 +0100

    Exclude idea from deployment

--- scripts/deploy-nostriot.com.sh
+++ scripts/deploy-nostriot.com.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 nvm use
 npm run build
-rsync -avz .  ubuntu@nostriot.com:nostriot.com --exclude=.next --exclude=.env --exclude=node_modules --exclude=.git
+rsync -avz .  ubuntu@nostriot.com:nostriot.com --exclude=.next --exclude=.env --exclude=node_modules --exclude=.git --exclude=.idea
 ssh ubuntu@nostriot.com "cd nostriot.com; npm install; npm run build;  pm2 restart nextjs-app"
